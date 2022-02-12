859ad7bfaf78a645f5095f0c496681093e825e17
Lucas Loyola <lucas.loyolamatos@gmail.com>
2022-02-12 16:33:32 -0300

    started work

---
+++ _javascript/function.js
@@ -0,0 +1,24 @@
+/* function formatation */
+
+function carregar() {
+   var msg = window.document.getElementById('msg');
+   var img = window.document.getElementById('imagem');
+   var data = new Date();
+   var hora = data.getHours();
+   msg.innerHTML = `Agora são ${hora} horas.`;
+
+   if (hora >= 0 && hora < 12) {
+      img.setAttribute = 'src', 'morning.png';
+      document.body.style.background = '#e2cd9f';
+   }
+
+   else if (hora >= 12 && hora <= 18) {
+      img.setAttribute = 'src', 'evening.png';
+      document.body.style.background = '#b9846f';
+   }
+
+   else {
+      img.setAttribute = 'src', 'night.png';
+      document.body.style.background = '#515154';
+   }
+}
